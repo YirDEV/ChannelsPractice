adddb1320a3e5139aa5bfd8138367f44e1e65ffc
YirDev <apjair97@gmail.com>
2020-09-04 03:44:07 -0500

    Start Channels practice repository

---
+++ xht_chat/urls.py
@@ -0,0 +1,9 @@
+# chat/urls.py
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'), # in this way you can create a variable path 
+]
